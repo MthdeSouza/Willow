b6a16fdd1764747ed24d4eab3c332053a96f5908
Matheus Cometti <matheus.alex@hotmail.com>
2020-08-14 09:31:30 -0300

    template implementation

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('woodwork app is running!');
+    expect(page.getTitleText()).toEqual('willow app is running!');
   });
 
   afterEach(async () => {
